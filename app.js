9cc3befe2c69b16290b4f557080ecc1e167efb93
Fattah Muhyiddeen <fattahmuhyiddeen@gmail.com>
2020-08-09 12:07:05 +0800

    bug fix

--- app.js
+++ app.js
@@ -15,6 +15,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const db_url = process.env.DATABASE_URL;
 
+const response = (responseObj, data, err) => {
+  if (err) {
+    responseObj.status(400);
+    responseObj.send(err)
+  } else {
+    responseObj.send({ data });
+  }
+}
+
 const client = new Client({
   ssl: { rejectUnauthorized: false },
   connectionString: db_url,
@@ -25,7 +34,7 @@ const COLUMNS = ['id', 'name', 'price', 'duration', 'validity', 'description']
 const port = process.env.PORT || 3001;
 
 app.get('/hotels', (req, res) => {
-  client.query('SELECT * FROM hotels ORDER BY created_at DESC', (err, query) => res.send(err || query.rows));
+  client.query('SELECT * FROM hotels ORDER BY created_at DESC', (err, query) => response(res, query.rows, err));
 })
 app.post('/hotel', (req, res) => {
   const { name, price, duration, validity, description } = req.body;
@@ -39,12 +48,12 @@ app.post('/hotel', (req, res) => {
   })
 
   const stmt = `INSERT INTO hotels(${COLUMNS.toString()}) VALUES(${stringValues}) RETURNING *`;
-  client.query(stmt, err => res.send(err || 'success'));
+  client.query(stmt, err => response(res, 'success', err));
 });
 
 app.delete('/hotel/:id', function (req, res) {
   const stmt = `DELETE FROM hotels WHERE id='${req.params.id}'`;
-  client.query(stmt, err => res.send(err || 'success'));
+  client.query(stmt, err => response(res, 'success', err));
 });
 
 
@@ -54,7 +63,7 @@ app.put('/hotel/:id', function (req, res) {
   const stmt = `UPDATE hotels 
     SET name='${name}', price=${price}, duration='${duration}', validity='${validity}', description='${description}'
     WHERE id='${req.params.id}'`;
-  client.query(stmt, err => res.send(err || 'success'));
+  client.query(stmt, err => response(res, 'success', err));
 });
 
 app.listen(port, () => {
